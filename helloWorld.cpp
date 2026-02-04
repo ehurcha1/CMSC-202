81cabdb4d29548cfec53eac19aad1a3590f0fd6e
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-03 23:11:09 -0500

    Add calculations and output to helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,5 +3,10 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    int num = 5;
+    int numNum = 9;
+    int numNumNum = num * numNum;
+    std::cout << numNumNum;
+    std::cout << "";
     return 0;
 }
