fc204a12e314676e050aec6ea128826191a3ed1e
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-07 18:18:24 -0500

    Add initial implementation of Pokemon trainer selection program
    - Created a new C++ file for the program.
    - Implemented user input for trainer's name and age.
    - Added selection options for three starter Pokemon: Bulbasaur, Squirtle, and Charmander.
    - Included descriptions for each Pokemon upon selection.
    - Implemented basic age validation for trainer eligibility.

---
+++ labs/lab1/lab1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int age=0;
+    int teamSelect=0;
+    int teamNum=3;
+
+    cout << "What is your name?" << endl;
+    cin >> name;
+    
+    cout << "What is your age?" << endl;
+    cin >> age;
+
+    if (10<=age<=122){
+        cout << "Welcome, Pokemon trainer " << name << "!" << endl;
+        cout << "To start your adventure, you must choose the first" << endl
+                << "member of your team. Take a minute to review your" << endl
+                << "options!" << endl;
+        cout << "What would you like to do?" << endl
+            << "1. Learn about Bulbasaur" << endl
+            << "2. Learn about Squirtle" << endl
+            << "3. Learn about Charmander" << endl;
+        cin >> teamSelect;
+    }else{
+        return 0;
+    }
+    while (teamNum != 0){
+        bool team1=true;
+        bool team2=true;
+        bool team3=true;
+
+        if (teamSelect == 1 && team1 == true){
+            team1 = false;
+            teamNum -= 1;
+            cout << "It carries a seed on its back right from birth. By" << endl
+                << "soaking up the sun's rays, the seed grows" << endl
+                << "progressively larger. It is filled with nutrients." << endl
+                << "Able to go for days without eating a single morsel, it" << endl
+                << "uses this to grow while it is young." << endl;
+        }else if (teamSelect == 2 && team2 == true){
+            team2 = false;
+            teamNum -= 1;
+            cout << "Its shell is soft immediately after it is born. In no" << endl
+                << "time at all, the shell becomes so resilient that a" << endl
+                << "prodding finger will bounce right off it. It hides in" << endl
+                << "its shell to protect itself, then strikes back with" << endl
+                << "spouts of water at every opportunity." << endl;
+        }else if (teamSelect == 3 && team3 == true){
+            team3 = false;
+            teamNum -= 1;
+            cout << "From the time it is born, a flame burns at the tip of" << endl
+                << "its tail. It shows the strength of its life-force. If" << endl
+                << "Charmander is weak, the flame also burns weakly. If" << endl
+                << "Charmander is healthy, the flame will burn vigorously" << endl
+                << "and won't go out even if it gets a bit wet." << endl;
+        }else{
+            cout << "Oh no! It seems that Professor Oak has run out of" << endl
+                << "starter Pokemon. Better luck next time!" << endl;
+        }
+    }
+
+    return 0;
+}
