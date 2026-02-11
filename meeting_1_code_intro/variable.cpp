3ea413b2022abcb213e0d8407d94b93dc7c20a5b
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-11 18:50:19 -0500

    Refactor and add new C++ files for various programming concepts
    - Removed Makefile and example code files that were no longer needed.
    - Added new C++ files demonstrating basic concepts such as:
      - Hello World program
      - Variable declaration and usage
      - Escape sequences and formatted output
      - Precision in arithmetic operations
      - Loops and switch statements
      - Functions and file I/O
      - Arrays and C-strings
      - Pointers and memory management
    - Included comments for clarity and educational purposes.

---
+++ meeting_1_code_intro/variable.cpp
@@ -0,0 +1,25 @@
+/*
+** Title: variable.cpp
+** Author: Jeremy Dixon
+** Date: 8/30/2023
+** Description: This introduces variables in C++
+** Course/Section: CMSC 202
+*/
+
+#include <iostream> //includes cin and cout
+#include <string> //includes string
+using namespace std;
+
+int main () {
+  int age = 10; //declare an integer variable and initialize to 10
+  cout << "What is your age?" << endl;
+  cin >> age;
+  cout << "You are " << age << " years old" << endl;
+
+  char myChar = ' ';
+  bool isHappy = true;
+  double myMoney = 100.01;
+  string myName = "Jeremy Dixon";
+  
+  return 0;
+}
