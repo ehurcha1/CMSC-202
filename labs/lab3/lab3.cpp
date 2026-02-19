fca7204252090d633734c667003630ad5f4528bb
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-19 13:32:44 -0500

    Enhance user prompts for input validation in lab3.cpp

--- labs/lab3/lab3.cpp
+++ labs/lab3/lab3.cpp
@@ -33,18 +33,18 @@ int main() {
   
   // Populate Array with unique, non-zero numbers from user for size NUM_NUMBERS
   for (int i = 0; i < NUM_NUMBERS; i++)
-    cout << "Enter a number: ";
+    cout << "Enter a unique number (not including 0): ";
     cin >> input;
 
     //if the input is 0, notifies user and re-prompts
     if (input == 0) {
-      cout << "Number cannot be zero. Enter a number: ";
+      cout << "is an invalid input, try another number: ";
       cin >> input;
     }
 
     //if the input is not unique (already in array), notifies user and re-prompts
     if (isUnique(input, myArray) == false){
-      cout << "Number already exists. Enter a number: ";
+      cout << "This number already exists in the array, try another number: ";
       cin >> input;
     //Otherwise, adds to the array
     }else{
@@ -52,14 +52,13 @@ int main() {
     }
   
   //Iterate through array and display all numbers (not sorted)
-  cout << "The numbers in the array are: ";
   for (int i = 0; i < NUM_NUMBERS; i++){
     cout << myArray[i] << " ";
   }
   cout << endl;
   
   //Display the number of even numbers in array using getEvenCount
-  cout << "Number of even numbers: " << getEvenCount(myArray) << endl;
+  cout << "Number of even numbers in the array is: " << getEvenCount(myArray) << endl;
   
   return 0;
 }
@@ -67,9 +66,8 @@ int main() {
 
 // Implementation of function that checks input uniqueness (true if unique else false)
 // isUnique is provided
+//parses through array and checks if passed value exists in array already
 bool isUnique(int input, int array1[]){
-
-  //parses through array and checks if passed value exists in array already
   for (int i = 0; i < NUM_NUMBERS; i++){
     if (input == array1[i])
       return false;
