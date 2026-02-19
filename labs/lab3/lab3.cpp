ec1fd64f762f5876562159c9968cd74e30fc748b
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-19 13:28:54 -0500

    Implement main and getEvenCount functions in lab3.cpp

---
+++ labs/lab3/lab3.cpp
@@ -0,0 +1,89 @@
+/*****************************************
+ ** File:    lab3.cpp
+ ** Project: CMSC 202 Lab 3, Spring 2026
+ **
+ ** Lab 3 involves using arrays. In this file you need to
+ ** implement the main() and getEvenCount() functions 
+ ***********************************************/
+
+#include <iostream>
+using namespace std;
+
+//Global Constants
+const int NUM_NUMBERS = 4; //Size of array and count of numbers to read
+
+// Function Name: isUnique
+// Pre-conditions: Requires a new number, and an array of numbers
+// Post-conditions: Returns true if the new number is not in the array, else false
+// PROVIDED Below
+bool isUnique(int, int[]);
+
+// Function Name: getEvenCount
+// Pre-conditions: Requires an array
+// Post-conditions: Returns the number of even numbers in the array
+int getEvenCount(int[]);
+
+// Implement main HERE*********
+int main() {
+  // Declare your necessary variable(s)
+  int input;
+  int i = 0;
+  // Declare array (myArray)
+  int myArray[NUM_NUMBERS] = {0};
+  
+  // Populate Array with unique, non-zero numbers from user for size NUM_NUMBERS
+  for (int i = 0; i < NUM_NUMBERS; i++)
+    cout << "Enter a number: ";
+    cin >> input;
+
+    //if the input is 0, notifies user and re-prompts
+    if (input == 0) {
+      cout << "Number cannot be zero. Enter a number: ";
+      cin >> input;
+    }
+
+    //if the input is not unique (already in array), notifies user and re-prompts
+    if (isUnique(input, myArray) == false){
+      cout << "Number already exists. Enter a number: ";
+      cin >> input;
+    //Otherwise, adds to the array
+    }else{
+      myArray[i] = input;
+    }
+  
+  //Iterate through array and display all numbers (not sorted)
+  cout << "The numbers in the array are: ";
+  for (int i = 0; i < NUM_NUMBERS; i++){
+    cout << myArray[i] << " ";
+  }
+  cout << endl;
+  
+  //Display the number of even numbers in array using getEvenCount
+  cout << "Number of even numbers: " << getEvenCount(myArray) << endl;
+  
+  return 0;
+}
+
+
+// Implementation of function that checks input uniqueness (true if unique else false)
+// isUnique is provided
+bool isUnique(int input, int array1[]){
+
+  //parses through array and checks if passed value exists in array already
+  for (int i = 0; i < NUM_NUMBERS; i++){
+    if (input == array1[i])
+      return false;
+  }
+  return true;
+}
+
+// Implement getEvenCount here
+// Function that returns the number of even numbers in an array
+int getEvenCount(int myArray[]){
+  int evenCount = 0;
+  for (int i = 0; i < NUM_NUMBERS; i++){
+    if (myArray[i] % 2 == 0)
+      evenCount++;
+  }
+  return evenCount;
+}
