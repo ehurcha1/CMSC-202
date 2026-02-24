14d50e4b2b8a7bab88905fff40074e83a27bbd8a
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-02-24 10:35:07 -0500

    Refine program description and update grid size constants in proj1.cpp

--- projects/proj1/proj1.cpp
+++ projects/proj1/proj1.cpp
@@ -7,7 +7,7 @@
 ** E-mail:  ehurcha1@umbc.edu
 **
 **This program will read an input file that has a 10x10 grid of letters
-**and a list of 6 letter words. It then displays the grid with numbered rows and columns for user input.
+**and a list of 6 words. It then displays the grid with numbered rows and columns for user input.
 **It also displays the list of words in the grid.
 **The user will then be prompted to enter the coordinates of the first and last letter of a word in the grid.
 **Input will be checked for validity.
@@ -16,7 +16,54 @@
 *****************************************/
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-//Global Constants
-const int GRID_SIZE = 10; // Size of the grid
+//Constants
+const int GRID_Y = 10; // Size of the grid
+const int GRID_X = 10; // Size of the grid
+
+//Function Prototypes
+
+//Reads file and returns grid array and words as two separate lists
+
+//Displays the 10x10 grid with numbered rows and columns. Top left is 0x0, bottom right is 9x9
+
+//Displays all remaining words in the list
+
+//Checks if user input is a valid coordinate
+
+//Takes start and end coords to determine if row, column, or diagonal
+
+//Builds the word by walking through grid from start to end coords
+
+//Checks if the built word is in the list of words
+
+//Removes the word from the list if it is found
+
+int main(){
+    //Declare necessary variables
+
+    //Read file and populate grid and word list
+
+    //Display grid and word list
+
+    //While there are still words in the list
+        //Prompt user for start and end coordinates
+
+        //Check if input is valid
+
+        //If valid, determine if row, column, or diagonal
+
+        //Build the word from the grid
+
+        //Check if the built word is in the list
+
+        //If it is, remove it from the list and notify user of success
+        //Else, notify user of failure
+
+    //Notify user of success and end program
+    return 0;
+}
+
+//Function definitions go here
