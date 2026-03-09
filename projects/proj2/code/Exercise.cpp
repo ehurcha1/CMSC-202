4b0f2efc056bbf658937f13984b990d8a6dae4ac
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-03-09 14:41:47 -0400

    Add initial implementation of Exercise class with user prompts and incomplete GetName method

---
+++ projects/proj2/code/Exercise.cpp
@@ -0,0 +1,13 @@
+//Title: Exercise.cpp
+//Author: Elliot Hurchalla
+//Date: 3/7/2026
+//Description: This is part of the project in CMSC 202.
+
+#include "Exercise.h"
+#include "fstream"
+
+Exercise::Exercise() {
+
+
+}
+
