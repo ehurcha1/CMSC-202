b5447cc835bf60f6eaaf45f87e24a46c0868796c
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-03-09 22:46:48 -0400

    Refactor Exercise class constructors and add InstallationLog.txt for installation details

--- projects/proj2/code/Exercise.cpp
+++ projects/proj2/code/Exercise.cpp
@@ -4,10 +4,22 @@
 //Description: This is part of the project in CMSC 202.
 
 #include "Exercise.h"
-#include "iostream"
 
+//Initialize with set zero values
 Exercise::Exercise() {
-
-
+    m_name = "";
+    m_muscle = "";
+    m_duration = 0;
 }
 
+//assign given values to m variables
+Exercise::Exercise(string name, string muscle, int duration) {
+    m_name = name;
+    m_muscle = muscle;
+    m_duration = duration;
+}
+
+//return m variables
+string Exercise::GetName() const {return m_name;}
+string Exercise::GetMuscle() const {return m_muscle;}
+int Exercise::GetDuration() const {return m_duration;}
---
+++ projects/proj2/code/WorkoutPlan.cpp
@@ -0,0 +1,6 @@
+//Title: WorkoutPlan.cpp
+//Author: Elliot Hurchalla
+//Date: 3/7/2026
+//Description: This is part of the project in CMSC 202.
+
+#include 
