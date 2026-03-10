6a1556dd602a55ddf5d29f15a812c060cc2c603e
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-03-10 14:13:20 -0400

    Clean up formatting and add spaces in Exercise.h comments and header guards

--- projects/proj2/code/Exercise.cpp
+++ projects/proj2/code/Exercise.cpp
@@ -1,25 +1,45 @@
-//Title: Exercise.cpp
-//Author: Elliot Hurchalla
-//Date: 3/7/2026
-//Description: This is part of the project in CMSC 202.
+// Title: Exercise.cpp
+// Author: Elliot Hurchalla
+// Date: 3/7/2026
+// Description: This is part of the project in CMSC 202.
 
 #include "Exercise.h"
 
-//Initialize with set zero values
+// Name - Exercise()
+// Desc - Default Constructor - Creates an empty exercise with default values
+// Preconditions - None
+// Postconditions - Exercise object is initialized with blank/zero data
 Exercise::Exercise() {
-    m_name = "";
-    m_muscle = "";
-    m_duration = 0;
+  m_name = "";
+  m_muscle = "";
+  m_duration = 0;
 }
 
-//assign given values to m variables
+// Name - Exercise(string name, string muscle, int duration)
+// Desc - Overloaded Constructor - Creates an exercise with provided name,
+//        muscle group, and duration in minutes
+// Preconditions - Caller provides exercise data values
+// Postconditions - Exercise object is initialized with provided data
 Exercise::Exercise(string name, string muscle, int duration) {
-    m_name = name;
-    m_muscle = muscle;
-    m_duration = duration;
+  m_name = name;
+  m_muscle = muscle;
+  m_duration = duration;
 }
 
-//return m variables
-string Exercise::GetName() const {return m_name;}
-string Exercise::GetMuscle() const {return m_muscle;}
-int Exercise::GetDuration() const {return m_duration;}
+// Name - GetName()
+// Desc - Returns the exercise name
+// Preconditions - Exercise object exists
+// Postconditions - Exercise name is returned to caller
+string Exercise::GetName() const { return m_name; }
+
+// Name - GetMuscle()
+// Desc - Returns the target muscle group
+// Preconditions - Exercise object exists
+// Postconditions - Muscle group is returned to caller
+string Exercise::GetMuscle() const { return m_muscle; }
+
+// Name - GetDuration()
+// Desc - Returns the exercise duration in minutes
+// Preconditions - Exercise object exists
+// Postconditions - Duration value is returned to caller
+int Exercise::GetDuration() const { return m_duration; }
--- projects/proj2/code/WorkoutPlan.cpp
+++ projects/proj2/code/WorkoutPlan.cpp
@@ -1,40 +1,55 @@
-//Title: WorkoutPlan.cpp
-//Author: Elliot Hurchalla
-//Date: 3/7/2026
-//Description: This is part of the project in CMSC 202.
+// Title: WorkoutPlan.cpp
+// Author: Elliot Hurchalla
+// Date: 3/7/2026
+// Description: This is part of the project in CMSC 202.
 
 #include "WorkoutPlan.h"
 
+// Name - WorkoutPlan()
+// Desc - Default Constructor - Creates a weekly workout plan and
+//        initializes day names
+// Preconditions - None
+// Postconditions - Day labels are stored for all 7 days
 WorkoutPlan::WorkoutPlan() {
-    m_dayNames[0] = "Monday";
-    m_dayNames[1] = "Tuesday";
-    m_dayNames[2] = "Wednesday";
-    m_dayNames[3] = "Thursday";
-    m_dayNames[4] = "Friday";
-    m_dayNames[5] = "Saturday";
-    m_dayNames[6] = "Sunday";
+  m_dayNames[0] = "Monday";
+  m_dayNames[1] = "Tuesday";
+  m_dayNames[2] = "Wednesday";
+  m_dayNames[3] = "Thursday";
+  m_dayNames[4] = "Friday";
+  m_dayNames[5] = "Saturday";
+  m_dayNames[6] = "Sunday";
 }
 
+// Name - SetDayExercise(int day, const Exercise &exercise)
+// Desc - Assigns an exercise to a given day index in the week
+// Preconditions - day must be in range 0 to 6
+// Postconditions - Matching day stores the provided exercise
 void WorkoutPlan::SetDayExercise(int day, const Exercise &exercise) {
-    if (day >= 0 && day < 7) {
-        m_weekPlan[day] = exercise;
-    }
+  if (day >= 0 && day < 7) {
+    m_weekPlan[day] = exercise;
+  }
 }
 
+// Name - GetDayExercise(int day)
+// Desc - Returns the exercise from a specific day
+// Preconditions - Day must be in range 0 to 6
+// Postconditions - Matching day returns the provided exercise
 Exercise WorkoutPlan::GetDayExercise(int day) const {
-    if (day >= 0 && day < 7) {
-        return m_weekPlan[day];
-    }
-    return Exercise();
+  if (day >= 0 && day < 7) {
+    return m_weekPlan[day];
+  }
+  return Exercise();
 }
 
+// Name - PrintPlan()
+// Desc - Displays the 7-day workout plan to the console
+// Preconditions - Plan may contain assigned exercises
+// Postconditions - Weekly plan text is output to standard output
 void WorkoutPlan::PrintPlan() const {
-    for (int i = 0; i >= 0 && i < 7; i++) {
-        cout << "=== Your 7-Day CMSC 202 Workout Plan ===" << endl;
-        cout << m_dayNames[i] << ":"
-        << m_weekPlan[i].GetName() << " ("
-        << m_weekPlan[i].GetMuscle() << ", "
-        << m_weekPlan[i].GetDuration() << " min)"
-        << endl;
-    }
+  for (int i = 0; i >= 0 && i < 7; i++) {
+    cout << "=== Your 7-Day CMSC 202 Workout Plan ===" << endl;
+    cout << m_dayNames[i] << ":" << m_weekPlan[i].GetName() << " ("
+         << m_weekPlan[i].GetMuscle() << ", " << m_weekPlan[i].GetDuration()
+         << " min)" << endl;
+  }
 }
