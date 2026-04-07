88aeaa9baf7b02804f626de87aeb1a5704d00924
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-07 16:37:57 -0400

    Refactor Car class and main function for improved readability and structure

--- practice/classes.cpp
+++ practice/classes.cpp
@@ -1,83 +1,75 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-
 
 class Car {
 public:
-	Car() {
-		m_owner = "";
-		m_year = 0;
-		m_make = "";
-		m_model = "";
-	}
+  Car() {
+    m_owner = "";
+    m_year = 0;
+    m_make = "";
+    m_model = "";
+  }
 
-	void setOwner(string owner) {
-		m_owner = owner;
-	}
+  void setOwner(string owner) { m_owner = owner; }
 
-	void setYear(int year) {
-		m_year = year;
-	}
+  void setYear(int year) { m_year = year; }
 
-	void setMake(string make) {
-		m_make = make;
-	}
+  void setMake(string make) { m_make = make; }
 
-	void setModel(string model) {
-		m_model = model;
-	}
+  void setModel(string model) { m_model = model; }
 
-	string getOwner() {
-		return m_owner;
-	}
+  string getOwner() { return m_owner; }
 
-	int getYear() {
-		return m_year;
-	}
+  int getYear() { return m_year; }
 
-	string getMake() {
-		return m_make;
-	}
+  string getMake() { return m_make; }
 
-	string getModel() {
-		return m_model;
-	}
+  string getModel() { return m_model; }
 
 private:
-	string m_owner;
-	int m_year;
-	string m_make;
-	string m_model;
+  string m_owner;
+  int m_year;
+  string m_make;
+  string m_model;
 };
 
-
 int main() {
+  vector<Car> garage;
+  Car theCar;
+  string active;
 
-	Car theCar;
+  while (active != "quit") {
+    cout << "\nWhat is your first name? ";
+    string name;
+    cin >> name;
+    theCar.setOwner(name);
 
+    cout << "\nWhat year is your car? ";
+    int year;
+    cin >> year;
+    theCar.setYear(year);
 
-	cout << "\nWhat is your first name? ";
-	string name;
-	cin >> name;
-	theCar.setOwner(name);
+    cout << "\nWhat make is your car? ";
+    string make;
+    cin >> make;
+    theCar.setMake(make);
 
-	cout << "\nWhat year is your car? ";
-	int year;
-	cin >> year;
-	theCar.setYear(year);
+    cout << "\nWhat model is your car? ";
+    string model;
+    cin >> model;
+    theCar.setModel(model);
 
-	cout << "\nWhat make is your car? ";
-	string make;
-	cin >> make;
-	theCar.setMake(make);
+    garage.push_back(theCar);
 
-	cout << "\nWhat model is your car? ";
-	string model;
-	cin >> model;
-	theCar.setModel(model);
+    cout << "\nWould you like to add another car? (type quit if finished) "
+         << endl;
+    cin >> active;
+  }
 
-	cout << "\n" << theCar.getOwner() << " drives a " << theCar.getYear() << " " << theCar.getMake() << " " << theCar.getModel() << endl;
-
-	return 0;
+  for (int i = 0; i < garage.size(); i++) {
+    cout << garage[i].getOwner() <<
+  }
+  return 0;
 }
