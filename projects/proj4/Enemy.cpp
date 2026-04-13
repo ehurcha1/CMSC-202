dc169b852a15e6cfbe0dc2fd67327d08a6116781
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-13 14:55:42 -0400

    n

--- projects/proj4/Enemy.cpp
+++ projects/proj4/Enemy.cpp
@@ -6,4 +6,28 @@ Enemy::Enemy(){
 	m_location = 0;
 }
 
+Enemy::Enemy(int health, int location){
+	m_health = health;
+	m_location = location;
+}
+
+Enemy::~Enemy(){
+//not used
+}
+
+int Enemy::GetHealth(){
+	return m_health;
+}
+
+void Enemy::SetHealth(int health){
+	m_health = health;
+}
+
+int GetLocation(){
+	return m_location;
+}
+
+void SetLocation(int location){
+	m_location = location;
+}
 
