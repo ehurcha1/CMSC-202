2a37c9df19288b89abda27de63ba1dd548d350e5
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-15 17:52:30 -0400

    Refactor MageTower class: streamline constructor and enhance attack method

--- projects/proj4/MageTower.cpp
+++ projects/proj4/MageTower.cpp
@@ -2,13 +2,39 @@
 using namespace std;
 
 MageTower::MageTower() : Tower("mage", 1, 0) {
-    SetType("mage");
-    SetDamage(1);
-    SetLocation(0);
+  SetType("mage");
+  SetDamage(1);
+  SetLocation(0);
 }
 
-MageTower::MageTower(string type, int damage, int location) : Tower(type, damage, location) {
-    SetType(type);
-    SetDamage(damage);
-    SetLocation(location);
+MageTower::MageTower(string type, int damage, int location)
+    : Tower(type, damage, location) {
+  SetType(type);
+  SetDamage(damage);
+  SetLocation(location);
 }
+
+int MageTower::Attack(vector<Enemy *> enemies) {
+  int gold = 0;
+
+  cout << "The Mage Tower launches an arcane bolt!" << endl;
+  for (int i = 0; i < enemies.size(); i++) {
+    int loc = enemies[i]->GetLocation();
+    if (loc == GetLocation() && enemies[i]->GetHealth() > 0) {
+      int dam = enemies[i]->TakeDamage(GetDamage());
+      gold += dam;
+    }
+  }
+
+  cout << "The arcane energy echoes back!" << endl;
+  for (int i = 0; i < enemies.size(); i++) {
+    int loc = enemies[i]->GetLocation();
+    if (loc == GetLocation() && enemies[i]->GetHealth() > 0) {
+      int dam = enemies[i]->TakeDamage(GetDamage());
+      gold += dam;
+    }
+  }
+  return gold;
+}
+
+MageTower::~MageTower() {}
