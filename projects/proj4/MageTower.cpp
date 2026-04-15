04e78fb11bee1da30161b3b88a218f8e69db4c03
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-15 11:08:22 -0400

    Implement MageTower constructors and initialize default values

--- projects/proj4/MageTower.cpp
+++ projects/proj4/MageTower.cpp
@@ -1,4 +1,14 @@
 #include "MageTower.h"
 using namespace std;
 
-MageTower::MageTower() : Tower() {}
+MageTower::MageTower() : Tower("mage", 1, 0) {
+    SetType("mage");
+    SetDamage(1);
+    SetLocation(0);
+}
+
+MageTower::MageTower(string type, int damage, int location) : Tower(type, damage, location) {
+    SetType(type);
+    SetDamage(damage);
+    SetLocation(location);
+}
