c2476a5d66200a8cead31f56560fdc4c692eee0d
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-15 22:32:44 -0400

    Add Artillery class implementation with constructors and attack method

---
+++ projects/proj4/Artillery.cpp
@@ -0,0 +1,31 @@
+#include "Artillery.h"
+using namespace std;
+
+Artillery::Artillery() : Tower("artillery", 1, 0) {
+  SetType("artillery");
+  SetDamage(1);
+  SetLocation(0);
+}
+
+Artillery::Artillery(string type, int damage, int location)
+    : Tower(type, damage, location) {
+  SetType(type);
+  SetDamage(damage);
+  SetLocation(location);
+}
+
+int Artillery::Attack(vector<Enemy *> enemies) {
+  int gold = 0;
+
+  cout << "The Artillery Tower launches a boulder! BOOM!" << endl;
+  for (int i = 0; i < enemies.size(); i++) {
+    int loc = enemies[i]->GetLocation();
+    if (loc == GetLocation() && enemies[i]->GetHealth() > 0) {
+      int dam = enemies[i]->TakeDamage(GetDamage());
+      gold += dam;
+    }
+  }
+  return gold;
+}
+
+Artillery::~Artillery() {}
