22ae052dd9a86ac92285034a1be61e3d1f1efb5c
Elliot Hurchalla <ehurcha1@umbc.edu>
2026-04-15 21:50:34 -0400

    Add ArcherTower class implementation with constructors and attack method

---
+++ projects/proj4/ArcherTower.cpp
@@ -0,0 +1,35 @@
+#include "ArcherTower.h"
+using namespace std;
+
+ArcherTower::ArcherTower() : Tower("archer", 1, 0) {
+  SetType("archer");
+  SetDamage(1);
+  SetLocation(0);
+}
+
+ArcherTower::ArcherTower(string type, int damage, int location)
+    : Tower(type, damage, location) {
+  SetType(type);
+  SetDamage(damage);
+  SetLocation(location);
+}
+
+int ArcherTower::Attack(vector<Enemy *> enemies) {
+  int gold = 0;
+  int i = 0;
+  bool found = false;
+  while (i < enemies.size() && found == false) {
+    int hp = enemies[i]->GetHealth();
+    int loc = enemies[i]->GetLocation();
+    if (hp > 0 && loc == GetLocation()) {
+      cout << "The Archer Tower fires an arrow!" << endl;
+      int dam = enemies[i]->TakeDamage(GetDamage());
+      gold += dam;
+      found = true;
+    }
+    i++;
+  }
+  return gold;
+}
+
+ArcherTower::~ArcherTower() {}
